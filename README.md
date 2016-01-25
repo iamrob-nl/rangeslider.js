a4560d555339e508949e58e5431a4738e4330583
rob duits <rob.duits@incentro.com>
2016-01-25 15:42:25 +0100

    modify rangeslider
    modify rangeslider to support onslide event and negative values
    (displayed as possitive) for statements.

--- README.md
+++ README.md
@@ -1,21 +1,103 @@
-#rangeslider.js [![Built with Grunt](https://cdn.gruntjs.com/builtwith.png)](http://gruntjs.com/) [![rangeslider.js](http://img.shields.io/badge/rangeslider-.js-00ff00.svg)](http://andreruffert.github.io/rangeslider.js/)
-Simple, small and fast JavaScript/jQuery polyfill for the HTML5 `<input type="range">` slider element.            
-Check out the [demo](http://andreruffert.github.io/rangeslider.js/). 
+# RangeSlider
 
+Provides a single range slider, that is fully responsive and works on desktop and mobile. This widget provides minimal styling out of the box so that there is maximum flexibiltiy. Please see the configuration section for more styling options.
 
-##Features
-* Touchscreen friendly
-* Recalculates `onresize` so suitable for use within responsive designs
-* Small and fast
+## Contributing
 
+For more information on contributing to this repository visit [Contributing to a GitHub repository](https://world.mendix.com/display/howto50/Contributing+to+a+GitHub+repository)!
 
-##Browser Support
-* Tested in IE8+ and all modern Browsers
+## Typical usage scenario
 
+Use this when you want to use a slider to select a single value for a input.
+ 
+## Configuration
 
-##Installation
-``bower install --save rangeslider.js``
+1. Select the value attribute to use.
+2. Select the min and max attributes to use.
 
+### Advanced
 
-##Usage
-Check out the [website's section](http://andreruffert.github.io/rangeslider.js/#usage) on how to use rangeslider.js.
+You can use default Bootstrap styling by selecting the appropriate classes for the fill and the handle; defaults are selected. An alternate styling that you can drop into your CSS theme to get the styling as shown on the source library.
+
+```
+.rangeslider,
+.rangeslider__fill {
+  -moz-box-shadow: inset 0px 1px 3px rgba(0, 0, 0, 0.3);
+  -webkit-box-shadow: inset 0px 1px 3px rgba(0, 0, 0, 0.3);
+  box-shadow: inset 0px 1px 3px rgba(0, 0, 0, 0.3);
+  -moz-border-radius: 10px;
+  -webkit-border-radius: 10px;
+  border-radius: 10px;
+}
+.rangeslider {
+  background: #e6e6e6;
+}
+.rangeslider__fill {
+  background: #00ff00;
+}
+.rangeslider__handle {
+  background: white;
+  border: 1px solid #ccc;
+  background-image: url('data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4gPHN2ZyB2ZXJzaW9uPSIxLjEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGRlZnM+PGxpbmVhckdyYWRpZW50IGlkPSJncmFkIiBncmFkaWVudFVuaXRzPSJvYmplY3RCb3VuZGluZ0JveCIgeDE9IjAuNSIgeTE9IjAuMCIgeDI9IjAuNSIgeTI9IjEuMCI+PHN0b3Agb2Zmc2V0PSIwJSIgc3RvcC1jb2xvcj0iI2ZmZmZmZiIgc3RvcC1vcGFjaXR5PSIwLjAiLz48c3RvcCBvZmZzZXQ9IjEwMCUiIHN0b3AtY29sb3I9IiMwMDAwMDAiIHN0b3Atb3BhY2l0eT0iMC4xIi8+PC9saW5lYXJHcmFkaWVudD48L2RlZnM+PHJlY3QgeD0iMCIgeT0iMCIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgZmlsbD0idXJsKCNncmFkKSIgLz48L3N2Zz4g');
+  background-size: 100%;
+  background-image: -webkit-gradient(linear, 50% 0%, 50% 100%, color-stop(0%, rgba(255, 255, 255, 0)), color-stop(100%, rgba(0, 0, 0, 0.1)));
+  background-image: -moz-linear-gradient(rgba(255, 255, 255, 0), rgba(0, 0, 0, 0.1));
+  background-image: -webkit-linear-gradient(rgba(255, 255, 255, 0), rgba(0, 0, 0, 0.1));
+  background-image: linear-gradient(rgba(255, 255, 255, 0), rgba(0, 0, 0, 0.1));
+  -moz-box-shadow: 0 0 8px rgba(0, 0, 0, 0.3);
+  -webkit-box-shadow: 0 0 8px rgba(0, 0, 0, 0.3);
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.3);
+  -moz-border-radius: 50%;
+  -webkit-border-radius: 50%;
+  border-radius: 50%;
+}
+.rangeslider-wrapper .rangeslider--horizontal .rangeslider__handle {
+	top: -12px;
+  width: 40px;
+  height: 40px;
+}
+.rangeslider-wrapper .rangeslider--vertical .rangeslider__handle {
+	left: -9px;
+  width: 40px;
+  height: 40px;
+}
+.rangeslider__handle:after {
+  content: "";
+  display: block;
+  width: 18px;
+  height: 18px;
+  margin: auto;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-image: url('data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4gPHN2ZyB2ZXJzaW9uPSIxLjEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGRlZnM+PGxpbmVhckdyYWRpZW50IGlkPSJncmFkIiBncmFkaWVudFVuaXRzPSJvYmplY3RCb3VuZGluZ0JveCIgeDE9IjAuNSIgeTE9IjAuMCIgeDI9IjAuNSIgeTI9IjEuMCI+PHN0b3Agb2Zmc2V0PSIwJSIgc3RvcC1jb2xvcj0iIzAwMDAwMCIgc3RvcC1vcGFjaXR5PSIwLjEzIi8+PHN0b3Agb2Zmc2V0PSIxMDAlIiBzdG9wLWNvbG9yPSIjZmZmZmZmIiBzdG9wLW9wYWNpdHk9IjAuMCIvPjwvbGluZWFyR3JhZGllbnQ+PC9kZWZzPjxyZWN0IHg9IjAiIHk9IjAiIHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIGZpbGw9InVybCgjZ3JhZCkiIC8+PC9zdmc+IA==');
+  background-size: 100%;
+  background-image: -webkit-gradient(linear, 50% 0%, 50% 100%, color-stop(0%, rgba(0, 0, 0, 0.13)), color-stop(100%, rgba(255, 255, 255, 0)));
+  background-image: -moz-linear-gradient(rgba(0, 0, 0, 0.13), rgba(255, 255, 255, 0));
+  background-image: -webkit-linear-gradient(rgba(0, 0, 0, 0.13), rgba(255, 255, 255, 0));
+  background-image: linear-gradient(rgba(0, 0, 0, 0.13), rgba(255, 255, 255, 0));
+  -moz-border-radius: 50%;
+  -webkit-border-radius: 50%;
+  border-radius: 50%;
+}
+.rangeslider__handle:active {
+  background-image: url('data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4gPHN2ZyB2ZXJzaW9uPSIxLjEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGRlZnM+PGxpbmVhckdyYWRpZW50IGlkPSJncmFkIiBncmFkaWVudFVuaXRzPSJvYmplY3RCb3VuZGluZ0JveCIgeDE9IjAuNSIgeTE9IjAuMCIgeDI9IjAuNSIgeTI9IjEuMCI+PHN0b3Agb2Zmc2V0PSIwJSIgc3RvcC1jb2xvcj0iIzAwMDAwMCIgc3RvcC1vcGFjaXR5PSIwLjEiLz48c3RvcCBvZmZzZXQ9IjEwMCUiIHN0b3AtY29sb3I9IiMwMDAwMDAiIHN0b3Atb3BhY2l0eT0iMC4xMiIvPjwvbGluZWFyR3JhZGllbnQ+PC9kZWZzPjxyZWN0IHg9IjAiIHk9IjAiIHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIGZpbGw9InVybCgjZ3JhZCkiIC8+PC9zdmc+IA==');
+  background-size: 100%;
+  background-image: -webkit-gradient(linear, 50% 0%, 50% 100%, color-stop(0%, rgba(0, 0, 0, 0.1)), color-stop(100%, rgba(0, 0, 0, 0.12)));
+  background-image: -moz-linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.12));
+  background-image: -webkit-linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.12));
+  background-image: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.12));
+}
+input[type="range"]:focus + .rangeslider .rangeslider__handle {
+  -moz-box-shadow: 0 0 8px rgba(255, 0, 255, 0.9);
+  -webkit-box-shadow: 0 0 8px rgba(255, 0, 255, 0.9);
+  box-shadow: 0 0 8px rgba(255, 0, 255, 0.9);
+}
+```
+
+## Dependencies
+
+1. jQuery v1.11.2
+2. rangeslider v2.0.2 [https://github.com/andreruffert/rangeslider.js](https://github.com/andreruffert/rangeslider.js)
---
+++ src/RangeSlider/widget/RangeSlider.js
@@ -0,0 +1,312 @@
+/*
+    RangeSlider
+    ========================
+
+    @file      : RangeSlider.js
+    @version   : 1.4.1
+    @author    : Chad Evans
+    @date      : 14 Oct 2015
+    @modified-by      : Rob Duits
+    @date-modified    : 22 jan 2016
+    @copyright : 2015, Mendix B.v.
+    @license   : Apache v2
+
+    Documentation
+    ========================
+    A Range Slider input control for Mendix. Based on https://github.com/andreruffert/rangeslider.js.
+*/
+
+// Required module list. Remove unnecessary modules, you can always get them back from the boilerplate.
+define([
+    "dojo/_base/declare",
+    "mxui/widget/_WidgetBase",
+    "dijit/_TemplatedMixin",
+    "mxui/dom",
+    "dojo/dom",
+    "dojo/dom-prop",
+    "dojo/query",
+    "dojo/dom-class",
+    "dojo/dom-style",
+    "dojo/dom-construct",
+    "dojo/_base/array",
+    "dojo/_base/lang",
+    "dojo/text",
+    "dojo/html",
+    "dojo/_base/event",
+    "RangeSlider/lib/jquery-1.11.2",
+    "RangeSlider/lib/rangeslider",
+    "dojo/text!RangeSlider/widget/template/RangeSlider.html"
+], function (declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoQuery, dojoClass, dojoStyle,
+    dojoConstruct, dojoArray, dojoLang, dojoText, dojoHtml, dojoEvent, _jQuery, _rangeslider, widgetTemplate) {
+    "use strict";
+
+    var $ = _jQuery.noConflict(true);
+
+    // Declare widget's prototype.
+    return declare("RangeSlider.widget.RangeSlider", [_WidgetBase, _TemplatedMixin], {
+        // _TemplatedMixin will create our dom node using this HTML template.
+        templateString: widgetTemplate,
+
+        // DOM elements
+        rangeSliderNode: null,
+        rangeInputNode: null,
+
+        // Parameters configured in the Modeler.
+        valueAttr: "",
+        minAttr: "",
+        maxAttr: "",
+        step: 10,
+        stepAttr: "",
+        orientation: "horizontal",
+        fillClass: "",
+        handleClass: "",
+        showValue: "true",
+        absoluteValue: "false",
+        onSlideEndMF: "",
+
+        // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
+        _handles: null,
+        _contextObj: null,
+        _alertDiv: null,
+        $rangeInputNode: null, //jQuery node
+        _options: null,
+        _initialized: false,
+
+        // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
+        constructor: function () {
+            this._handles = [];
+        },
+
+        // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
+        postCreate: function () {
+            //console.log(this.id + ".postCreate");
+
+            this._updateRendering();
+            this._setupEvents();
+        },
+
+        // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
+        update: function (obj, callback) {
+            //console.log(this.id + ".update");
+
+            this._contextObj = obj;
+            this._resetSubscriptions();
+            this._updateRendering();
+
+            callback();
+        },
+
+        // Attach events to HTML dom elements
+        _setupEvents: function () {
+            if (!this.stepAttr || this.stepAttr === "") {
+                dojoProp.set(this.rangeInputNode, {
+                    step: this.step
+                });
+            }
+            this._options = {
+                orientation: this.orientation,
+                polyfill: false,
+                fillClass: "rangeslider__fill " + this.fillClass,
+                handleClass: "rangeslider__handle " + this.handleClass,
+                onSlideEnd: dojoLang.hitch(this, this._onSlideEnd),
+                onSlide: dojoLang.hitch(this, this._onSlide)
+            };
+
+            dojoClass.add(this.rangeSliderNode, this.orientation);
+
+            this.connect(this.rangeInputNode, "change", function (e) {
+                // Function from mendix object to set an attribute.
+                this._contextObj.set(this.valueAttr, this.rangeInputNode.value);
+            });
+        },
+
+        // Rerender the interface.
+        _updateRendering: function () {
+            this.rangeInputNode.disabled = this.readOnly;
+
+            if (this._contextObj !== null) { 
+                dojoStyle.set(this.domNode, "display", "block");
+
+                if (this.stepAttr && this.stepAttr !== "") {
+                    dojoProp.set(this.rangeInputNode, {
+                        step: this._contextObj.get(this.stepAttr)
+                    });
+                }
+              
+                var valu = this._contextObj.get(this.valueAttr),
+                    min = this._contextObj.get(this.minAttr),
+                    max = this._contextObj.get(this.maxAttr);
+              
+                if (!this.$rangeInputNode) {
+                    this.$rangeInputNode = $(this.rangeInputNode);
+                }
+                this.$rangeInputNode.rangeslider(this._options);
+
+                if (!this._initialized) {
+                    // add some blank text to the fill and handle
+                    // needed when using label styling
+                    var fill = dojoQuery(".rangeslider__fill", this.rangeSliderNode);
+                    if (fill.length > 0) {
+                        dojoHtml.set(fill[0], "&nbsp;");
+                    }
+                  
+                    var handle = dojoQuery(".rangeslider__handle", this.rangeSliderNode);
+                  
+                    if (handle.length > 0) {
+                      if(this.showValue === "true" && this.absoluteValue === "true") {
+                        dojoHtml.set(handle[0], "<span>"+Math.abs(valu)+"</span>");
+                      } else if(this.showValue === "true" && this.absoluteValue === "false") {
+                        dojoHtml.set(handle[0], "<span>"+valu+"</span>");
+                      } else if(this.showValue === "false") {
+                        dojoHtml.set(handle[0], "<span></span>");
+                      }
+                    }
+                }
+
+                var _this = this;
+                return setTimeout(function () {
+                    dojoProp.set(_this.rangeInputNode, {
+                        min: min,
+                        max: max
+                    });
+                  
+                    _this.$rangeInputNode.val(valu).change();
+
+                    _this.$rangeInputNode.rangeslider("update", true);
+                }, 10);
+            } else {
+                dojoStyle.set(this.domNode, "display", "none");
+
+                if (this.$rangeInputNode) {
+                    this.$rangeInputNode.rangeslider("destroy");
+
+                    this.$rangeInputNode = null;
+                }
+            }
+
+            // Important to clear all validations!
+            this._clearValidations();
+        },
+      
+        _onSlide: function (value) {
+            var handle = dojoQuery(".rangeslider__handle", this.rangeSliderNode);
+              if (handle.length > 0) {
+                if(this.showValue === "true" && this.absoluteValue === "true") {
+                  dojoHtml.set(handle[0], "<span>"+Math.abs(this.rangeInputNode.value)+"</span>");
+                } else if(this.showValue === "true" && this.absoluteValue === "false") {
+                  dojoHtml.set(handle[0], "<span>"+this.rangeInputNode.value+"</span>");
+                } else if(this.showValue === "false") {
+                  dojoHtml.set(handle[0], "<span></span>");
+                }
+            }
+        },
+
+        _onSlideEnd: function (position, value) {
+            this._contextObj.set(this.valueAttr, this.rangeInputNode.value);
+            if (this.onSlideEndMF) {
+                mx.data.action({
+                    params: {
+                        applyto: "selection",
+                        actionname: this.onSlideEndMF,
+                        guids: [this._contextObj.getGuid()]
+                    },
+                    callback: function (obj) {
+                        //TODO what to do when all is ok!
+                    },
+                    error: dojoLang.hitch(this, function (error) {
+                        console.log(this.id + ": An error occurred while executing microflow: " + error.description);
+                    })
+                }, this);
+            }
+        },
+
+        // Handle validations.
+        _handleValidation: function (validations) {
+            //console.log(this.id + "._handleValidation");
+
+            this._clearValidations();
+
+            var validation = validations[0],
+                message = validation.getReasonByAttribute(this.valueAttr);
+
+            if (this.readOnly) {
+                validation.removeAttribute(this.valueAttr);
+            } else if (message) {
+                this._addValidation(message);
+                validation.removeAttribute(this.valueAttr);
+            }
+        },
+
+        // Clear validations.
+        _clearValidations: function () {
+            dojoConstruct.destroy(this._alertDiv);
+            this._alertDiv = null;
+        },
+
+        // Show an error message.
+        _showError: function (message) {
+            if (this._alertDiv !== null) {
+                dojoHtml.set(this._alertDiv, message);
+                return true;
+            }
+            this._alertDiv = dojoConstruct.create("div", {
+                "class": "alert alert-danger",
+                "innerHTML": message
+            });
+            dojoConstruct.place(this._alertDiv, this.domNode);
+        },
+
+        // Add a validation.
+        _addValidation: function (message) {
+            this._showError(message);
+        },
+
+        // Reset subscriptions.
+        _resetSubscriptions: function () {
+            // Release handles on previous object, if any.
+            if (this._handles) {
+                this._handles.forEach(function (handle) {
+                    mx.data.unsubscribe(handle);
+                });
+                this._handles = [];
+            }
+
+            // When a mendix object exists create subscribtions. 
+            if (this._contextObj) {
+                var objectHandle = this.subscribe({
+                    guid: this._contextObj.getGuid(),
+                    callback: dojoLang.hitch(this, function (guid) {
+                        this._updateRendering();
+                    })
+                });
+
+                var attrHandle = this._subscribeAttr(this.valueAttr);
+                var minAttrHandle = this._subscribeAttr(this.minAttr);
+                var maxAttrHandle = this._subscribeAttr(this.maxAttr);
+                var stepAttrHandle = this._subscribeAttr(this.stepAttr);
+
+                var validationHandle = this.subscribe({
+                    guid: this._contextObj.getGuid(),
+                    val: true,
+                    callback: dojoLang.hitch(this, this._handleValidation)
+                });
+
+                this._handles = [objectHandle, attrHandle, minAttrHandle, maxAttrHandle, stepAttrHandle, validationHandle];
+            }
+        },
+
+        _subscribeAttr: function (entityAttr) {
+            return this.subscribe({
+                guid: this._contextObj.getGuid(),
+                attr: entityAttr,
+                callback: dojoLang.hitch(this, function (guid, attr, attrValue) {
+                    this._updateRendering();
+                })
+            });
+        }
+    });
+});
+
+require(["RangeSlider/widget/RangeSlider"], function () {
+    "use strict";
+});
